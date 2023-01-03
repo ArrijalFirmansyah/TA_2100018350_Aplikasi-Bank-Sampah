287fb6127b52a109e4bf43727b3ef4347aefc068
Arrijal Firmansyah <101626045+ArrijalFirmansyah@users.noreply.github.com>
2023-01-03 18:51:47 +0700

    Delete Jenissampah.java

--- Revisi Bank Sampah/src/Jenissampah.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public abstract class Jenissampah implements Konsumen{
-    @Override
-    public abstract void setData();
-    @Override
-    public abstract void tampil();
-    public static Scanner in=new Scanner(System.in);
-}
